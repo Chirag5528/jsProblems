853a507be61af19361ce3319d3f4f225195cefea
Chirag5528 <chirag.arora5560@gmail.com>
2020-12-18 18:09:14 +0530

    Further More Changes

--- upload.php
+++ upload.php
@@ -3,8 +3,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fileName = $_POST['fileName'];
     $contentType = $_POST['contentType'];
     $base64 = $_POST['base64'];
-
     $data = base64_decode($base64);
-    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/vardhaman-accounts/images/test.pdf', $data);
+    $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/vardhman-accounts/images/testFile.pdf', "w");
+    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/vardhman-accounts/images/testFile.pdf', $data);
     die;
 }
